010a8f173952313ebf23a5c6567127ce21704181
TayaHatum <21taya21@gmail.com>
2023-03-20 21:44:54 +0200

    added negative login

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -1,8 +1,13 @@
 package manager;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class HelperUser extends HelperBase{
     public HelperUser(WebDriver wd) {
@@ -43,4 +48,21 @@ public class HelperUser extends HelperBase{
         click(By.xpath("//button[text()='Sign Out']"));
     }
 
+    public boolean isAlertPresent(String message) {
+        WebDriverWait wait = new WebDriverWait(wd, Duration.ofSeconds(5));
+
+        Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+        if(alert != null && alert.getText().equals(message)){
+            System.out.println(alert.getText());
+            // click ok
+            // pause
+            alert.accept();
+            // click cancel  --->  alert.dismiss();
+            // type into alert      -->alert.sendKeys("hello");
+
+
+            return true;
+        }
+        return false;
+    }
 }
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -4,40 +4,64 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class LoginTests extends TestBase{
+public class LoginTests extends TestBase {
 
-@BeforeMethod
-public void preCondition(){
+    @BeforeMethod
+    public void preCondition() {
 
-    if(app.getHelperUser().isLogged()){
-        app.getHelperUser().logout();
+        if (app.getHelperUser().isLogged()) {
+            app.getHelperUser().logout();
+        }
     }
-}
 
 
     @Test
-    public void loginSuccess(){
-       app.getHelperUser().openLoginRegistrationForm();
-       app.getHelperUser().fillLoginRegistrationForm("noa@gmail.com","Nnoa12345$");
-       app.getHelperUser().submitLogin();
-       // time ---> signOut
+    public void loginSuccess() {
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("noa@gmail.com", "Nnoa12345$");
+        app.getHelperUser().submitLogin();
+        // time ---> signOut
 
-       //Assert  is SignOup present?
-       // Assert.assertEquals(,);
-       // Assert.assertNotEquals();
-       // Assert.assertFalse();
+        //Assert  is SignOup present?
+        // Assert.assertEquals(,);
+        // Assert.assertNotEquals();
+        // Assert.assertFalse();
 
         Assert.assertTrue(app.getHelperUser().isLogged());
 
     }
 
     @Test
-    public void loginSuccessModel(){
+    public void loginSuccessModel() {
         app.getHelperUser().openLoginRegistrationForm();
-        app.getHelperUser().fillLoginRegistrationForm("noa@gmail.com","Nnoa12345$");
+        app.getHelperUser().fillLoginRegistrationForm("noa@gmail.com", "Nnoa12345$");
         app.getHelperUser().submitLogin();
 
         Assert.assertTrue(app.getHelperUser().isLogged());
+
+    }
+
+    @Test
+    public void loginWrongEmail(){
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("noagmail.com", "Nnoa12345$");
+        app.getHelperUser().submitLogin();
+        Assert.assertTrue(app.getHelperUser().isAlertPresent("Wrong email or password"));
+    }
+
+    @Test
+    public void loginWrongPassword(){
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("noa@gmail.com", "Nnoa12");
+        app.getHelperUser().submitLogin();
+        Assert.assertTrue(app.getHelperUser().isAlertPresent("Wrong email or password"));
+    }
+    @Test
+    public void loginUnregisteredUser(){
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("pop@gmail.com", "Pop12345$");
+        app.getHelperUser().submitLogin();
+        Assert.assertTrue(app.getHelperUser().isAlertPresent("Wrong email or password"));
 
     }
 }
