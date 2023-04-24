7e53c7c44ab7260fadd52ce970de0d2601017e57
TayaHatum <21taya21@gmail.com>
2023-04-24 20:57:26 +0300

    hw

--- src/main/java/manager/DataProviderUser.java
+++ src/main/java/manager/DataProviderUser.java
@@ -3,6 +3,7 @@ package manager;
 import models.User;
 import org.testng.annotations.DataProvider;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -20,6 +21,36 @@ public class DataProviderUser {
     @DataProvider
     public Iterator<Object[]> example(){
         List<Object[]> list= new ArrayList<>();
+
+        return list.iterator();
+    }
+    @DataProvider
+    public Iterator<Object[]> loginFile() throws IOException {
+        List<Object[]> list= new ArrayList<>();
+        // read from file --> add to list
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/data.csv")));
+       String line =  reader.readLine();  ///   "noa@gmail.com,Nnoa12345$"
+        while (line!=null){
+           String[] all= line.split(",");// ["noa@gmail.com"] ["Nnoa12345$"]
+            list.add(new Object[]{new User().withEmail(all[0]).withPassword(all[1])});
+            line=reader.readLine();   // "sonya@gmail.com,Ss12345$"   //"noa@gmail.com,Nnoa12345$"  // null
+        }
+
+
+        return list.iterator();
+    }
+    @DataProvider
+    public Iterator<Object[]> loginFile2() throws IOException {
+        List<Object[]> list= new ArrayList<>();
+        // read from file --> add to list
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/test.csv")));
+        String line =  reader.readLine();  ///   "noa@gmail.com,Nnoa12345$"
+        while (line!=null){
+            String[] all= line.split("/");// ["noa@gmail.com"] ["Nnoa12345$"]
+            list.add(new Object[]{new User().withEmail(all[0]).withPassword(all[1])});
+            line=reader.readLine();   // "sonya@gmail.com,Ss12345$"   //"noa@gmail.com,Nnoa12345$"  // null
+        }
+
 
         return list.iterator();
     }
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -47,6 +47,16 @@ public class LoginTests extends TestBase {
         Assert.assertTrue(app.getHelperUser().isLogged());
         logger.info("Assert check is Element button 'Sign out' present");
     }
+    @Test(dataProvider = "loginFile2",dataProviderClass = DataProviderUser.class)
+    public void loginSuccessModelDP(User user) {
+        logger.info("Test data ---> " +user.toString());
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm(user);
+        app.getHelperUser().submitLogin();
+
+        Assert.assertTrue(app.getHelperUser().isLogged());
+        logger.info("Assert check is Element button 'Sign out' present");
+    }
 
     @Test
     public void loginWrongEmail(){
