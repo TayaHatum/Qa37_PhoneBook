2088bc67696525af1e458be8f9ebe16964cc3491
TayaHatum <21taya21@gmail.com>
2023-04-27 20:53:33 +0300

    added browser & suite

--- src/main/java/manager/ApplicationManager.java
+++ src/main/java/manager/ApplicationManager.java
@@ -3,6 +3,9 @@ package manager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.Browser;
 import org.openqa.selenium.support.events.EventFiringDecorator;
 import org.openqa.selenium.support.events.WebDriverListener;
 import org.slf4j.Logger;
@@ -17,13 +20,31 @@ public class ApplicationManager {
     WebDriver wd;
     HelperUser helperUser;
     HelperContact helperContact;
+    String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init(){
 
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        wd = new ChromeDriver(options);
-        logger.info("All tests run in Chrome Browser");
+        if(browser.equals(Browser.CHROME.browserName())) {
+            // "firefox" "chrome"
+
+            Browser.EDGE.browserName();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--remote-allow-origins=*");
+            wd = new ChromeDriver(options);
+            logger.info("All tests run in Chrome Browser");
+        }else if(browser.equals(Browser.FIREFOX.browserName())){ // "firefox" == "firefox"
+            wd= new FirefoxDriver();
+            logger.info("All tests run in FIREFOX Browser");
+        }else if (browser.equals(Browser.EDGE.browserName())){
+            wd = new EdgeDriver();
+            logger.info("All tests run in EDGE Browser");
+        }
+
+
         WebDriverListener listener = new ListenerWD();
         wd=new EventFiringDecorator<>(listener).decorate(wd);
 
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,6 +1,7 @@
 package tests;
 
 import manager.ApplicationManager;
+import org.openqa.selenium.remote.Browser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterSuite;
@@ -11,7 +12,7 @@ import java.lang.reflect.Method;
 
 public class TestBase {
 Logger logger = LoggerFactory.getLogger(TestBase.class);
-   static ApplicationManager app = new ApplicationManager();
+   static ApplicationManager app = new ApplicationManager(System.getProperty("browser", Browser.CHROME.browserName()));
 
    @BeforeMethod
    public void startLogger(Method m){
@@ -22,7 +23,7 @@ Logger logger = LoggerFactory.getLogger(TestBase.class);
    public void setUp(){
        app.init();
    }
-@AfterSuite
+@AfterSuite(alwaysRun = true)
    public void tearDown(){
        app.stop();
    }
